51d7d879a442229936286d7852bde99e7a2fe09e
dli <davidli012345@gmail.com>
2019-12-30 18:10:49 -0800

    trying again

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	util.GetEnv("JWT_SECRET", "fail")
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = 8000
+		port = "8000"
 	}
 	// We create a simple server using http.Server and run.
 	server := &http.Server{
